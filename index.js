3b112338991f18691b7607c2a519d2bf02e45c6f
Victor <109470540+raviheima@users.noreply.github.com>
2023-10-24 00:58:07 +0100

    reacts easy setup

---
+++ index.js
@@ -0,0 +1,15 @@
+
+ReactDOM.render(
+  <>
+    <pre>
+      If everyone were rich, would the world be a peaceful place to live in?
+    </pre>
+    <ul>
+      <li>Peace to you all</li>
+      <li>If we decide to embrace our flaws and support each other, a lot will change.</li>
+      <li>Life is not all about having fun.</li>
+      <li>There needs to be a balance between morals and pleasure.</li>
+    </ul>
+  </>,
+  document.getElementById("root")
+);
