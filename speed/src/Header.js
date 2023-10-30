181e08318f2c4c470f73b41864f955c36d102ceb
raviheima <vctroreo@gmail.com>
2023-10-30 16:53:07 +0100

    added popper.js

--- speed/src/Header.js
+++ speed/src/Header.js
@@ -1,19 +1,19 @@
 import React from "react";
 import reactLogo from "./images/react.png";
-export default function Header(){
-    return(
-        <header>
-            <nav className="nav navbar navbar-dark bg-dark">
-            <img src={reactLogo} alt="react logo" className="nav-img" />
-            <h3 className="reactfacts">ReactFacts</h3>
-           
 
-                <ul className="nav-items">
-                    <li className="nav-item"Blog</li>
-                    <li className="nav-item">About </li>
-                    <li className="nav-item">Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header>
+      <nav className="nav navbar navbar-dark bg-dark">
+        <img src={reactLogo} alt="react logo" className="nav-img" />
+        <h3 className="reactfacts">ReactFacts</h3>
+
+        <ul className="nav-items">
+          <li className="nav-item">Blog</li>
+          <li className="nav-item">About</li>
+          <li className="nav-item">Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
--- speed/src/Maincontent.js
+++ speed/src/Maincontent.js
@@ -1,14 +1,14 @@
-import React from "react"
-export default function Maincontent(){
-    return(
-        <div className="container">
-             <h1 className="display-1">Reasons: why am learning react </h1>
-             <ol className="list-group">
-                <li className"list-group-item">React Is Cool</li>
-                <li className"list-group-item">React is fun to learn</li>
-                <li className"list-group-item">React is easy going</li>
-             </ol>
+import React from "react";
 
-        </div>
-    )
+export default function Maincontent() {
+  return (
+    <div className="container">
+      <h1 className="display-1">Reasons: why I am learning React</h1>
+      <ol className="list-group">
+        <li className="list-group-item">React Is Cool</li>
+        <li className="list-group-item">React is fun to learn</li>
+        <li className="list-group-item">React is easy going</li>
+      </ol>
+    </div>
+  );
 }
