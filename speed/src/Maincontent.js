e155a7e61ab2d50e539de00fb054251ac49989f9
raviheima <vctroreo@gmail.com>
2023-10-30 13:37:28 +0100

    i

--- speed/src/Maincontent.js
+++ speed/src/Maincontent.js
@@ -1,12 +1,12 @@
 import React from "react"
 export default function Maincontent(){
     return(
-        <div>
-             <h1>Reasons: why am learning react </h1>
-             <ol>
-                <li>React Is Cool</li>
-                <li>React is fun to learn</li>
-                <li>React is easy going</li>
+        <div className="container">
+             <h1 className="">Reasons: why am learning react </h1>
+             <ol className="list-group">
+                <li className"list-group-item">React Is Cool</li>
+                <li className"list-group-item">React is fun to learn</li>
+                <li className"list-group-item">React is easy going</li>
              </ol>
 
         </div>
